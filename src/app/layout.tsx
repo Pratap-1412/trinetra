a92c54914a2972b0fa994f33c01067d115079aa2
Abhay Pratap Mishra <118089139+Pratap-1412@users.noreply.github.com>
2025-09-26 20:55:20 +0530

    na

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { Suspense } from "react"
 
@@ -20,10 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}>
+      <body className={`font-sans antialiased`}>
         <Suspense>
           {children}
-          <Analytics />
         </Suspense>
       </body>
     </html>
